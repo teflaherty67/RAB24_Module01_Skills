defcdea4c2b973fd057605d8b1979e3c95c201ec
Tom E Flaherty <teflaherty@utexas.edu>
2024-08-13 22:48:23 -0500

    in progress

--- RAB24_Module01_Skills/Command1.cs
+++ RAB24_Module01_Skills/Command1.cs
@@ -11,7 +11,39 @@
             // this is a variable for the current Revit model
             Document doc = uiapp.ActiveUIDocument.Document;
 
-            // Your code goes here
+            // create a comment using a double forward slash
+            // comments to do get compiled
+            // used to leave yourself notes in your code
+
+            // let's create some variables
+            // DataType VariableName = Value; <- always end the line with a semicolon!
+
+            // create string variables
+            string text1 = "this is my text";
+            string text2 = "this is my next text";
+
+            // combine strings
+            string text3 = text1 + text2;
+            string text4 = text1 + " " + text2 + "abcd";
+
+            // create number variables
+            int number1 = 10;
+            double number2 = 20.5;
+
+            // do some math
+            double number3 = number1 + number2;
+            double number4 = number3 - number2;
+            double number5 = number4 / 100;
+            double number6 = number5 * number4;
+
+            // order of operations
+            double number7 = (number6 + number5) / number4;
+
+            // convert meters to feet
+            double meters = 4;
+            double metersToFeet = meters * 3.28084;
+
+             
 
             return Result.Succeeded;
         }
